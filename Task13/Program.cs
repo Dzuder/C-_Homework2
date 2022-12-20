0487f5ebb7f25a74a38728c393d2283770653a70
Dzuder <dzuder14@yandex.ru>
2022-12-20 20:31:57 +0500

    Create Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,6 @@
+﻿using static System.Console;
+WriteLine("Enter a number: ");
+int a = int.Parse(ReadLine()!);
+//WriteLine(a > 99? (a - ((a % 100) + (a % 10)) / 10) : "There are no third digits in the entered number") ;
+//WriteLine((a - (a % 100)) / 100);
+WriteLine(a > 99? (a % 1000 / 100) : "There are no third digits in the entered number");
