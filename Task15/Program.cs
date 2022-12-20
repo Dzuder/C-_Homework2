dd09c93c34feb4686e53596e0c69c01695746c8e
Dzuder <dzuder14@yandex.ru>
2022-12-20 23:09:17 +0500

    Creat task15

---
+++ Task15/Program.cs
@@ -0,0 +1,35 @@
+﻿using static System.Console;
+
+WriteLine("Enter number of the day of the week: ");
+int a = Convert.ToInt32(ReadLine()!);
+string b = "Day";
+// все что ниже с днями и переменная b, я сам решил усложнить для красивого ответа.
+if (a == 1) {
+   b = "Monday";
+}
+if (a == 2)
+{
+    b = "Tuesday";
+}
+if (a == 3)
+{
+    b = "Wednesday";
+}
+if (a == 4)
+{
+    b = "Tursday";
+}
+if (a == 5)
+{
+    b = "Friday";
+}
+if (a == 6)
+{
+    b = "Saturday";
+}
+if (a == 7)
+{
+    b = "Sunday";
+}
+WriteLine((a >= 6) && a <=7 ? "Yes, " + b + " is a weekend." : (a >=1 && a <= 5)? "No, " + b + " is not a weekend." : "In a week of 7 days, enter from 1 to 7." );
+
